cbf3eb018351f08edc27568b76adf2c0e7b3bedc
TayiebK <150594024+mtk2608@users.noreply.github.com>
2023-12-07 12:59:41 +0200

    Completed

---
+++ Assignment1.Sln/Assignment1/Lyrics.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Lyrics
+    {
+        
+        public static string Words()
+        {
+
+            for (int MilkNum = 99; MilkNum > 0; MilkNum--)
+
+            {
+                if (MilkNum > 1)
+                {
+                    Console.WriteLine($"{MilkNum}, bottles of beer on the wall, { MilkNum}, bottles of beer");
+
+                    if (MilkNum - 1 == 1)
+                    {
+                        Console.WriteLine($"Take one pass it around, {MilkNum}, bottle of beer on the wall.");
+
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"Take one pass it around, {MilkNum}, bottles of beer on the wall.");
+                    }
+
+                }
+
+                else if (MilkNum == 1)
+                {
+                    Console.WriteLine($"{MilkNum}, bottle of beer on the wall,{MilkNum},bottle of beer");
+
+                }
+            }
+            return "";
+        }
+
+       
+    }
+}
---
+++ Assignment1.Sln/Assignment1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Assignment1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string Output = Lyrics.Words();
+
+            Console.WriteLine(Output);
+        }
+    }
+}
